fbe066001af2bb9a28b126322fcbe3f4c4eeb95a
Rupinder Monga <rupindermonga@gmail.com>
2019-09-03 15:27:20 +0530

    Database imported from csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib','seeds','listings.csv'))
+csv = CSV.parse(csv_text, :headers => false, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = House.new
+  t.latitude = row[0]
+  t.longitude = row[1]
+  t.address = row[2]
+  t.save
+end
+
+puts "There are now #{House .count} rows in the transactions table"
