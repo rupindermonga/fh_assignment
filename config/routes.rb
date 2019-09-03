193f5ab6bbc5df43cdefd4832f246b7e679f6ef6
Rupinder Monga <rupindermonga@gmail.com>
2019-09-03 16:32:41 +0530

    Login/ sign_up set with basic css

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'dashboard#index'
   get :search, controller: :main
+#  root to: redirect('/search')
 
 end
